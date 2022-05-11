9e6776887d6acac6f7d64c66d2e5521f41240f28
joshuaspiral <82312141+joshuaspiral@users.noreply.github.com>
2022-05-12 07:15:41 +0800

    8.py refactor

--- 8.py
+++ 8.py
@@ -1,36 +1,18 @@
 import sys
 
-def encode(key, charset, msg):
-    encoded = ""
-    longKey = key * (len(msg) // len(key))
-    longKey = longKey + key[:len(msg) % len(key)]
-    for i in range(len(msg)):
-        if msg[i] in charset:
-            encoded += charset[(charset.index(longKey[i]) + 1 + charset.index(msg[i])) % len(charset)]
-        else:
-            encoded += msg[i]
-
-    return encoded
-
-
 def decode(key, charset, msg):
     decoded = ''
-    longKey = key * (len(msg) // len(key))
-    longKey = longKey + key[:len(msg) % len(key)]
     for i in range(len(msg)):
-        if msg[i] in charset:
-            decoded += (charset[(charset.index(msg[i]) - (charset.index(longKey[i]) + 1)) % len(charset)])
-        else:
-            decoded += msg[i]
-
+        if msg[i] in charset: decoded += (charset[(charset.index(msg[i]) - (charset.index(key[i % len(key)]) + 1)) % len(charset)]) 
+        else: decoded += msg[i]
     return decoded
 
 filename = '8.in' if len(sys.argv) < 2 else sys.argv[1]
+
 with open(filename) as f:
     l = f.read().splitlines()
 
-charset = [x for x in l[0]]
-key = l[1]
-msg = l[2]
-decoded = decode(key, charset, msg)
-print(decoded)
+charset, key, msg = l
+charset = [x for x in charset]
+
+print(decode(key, charset, msg))
